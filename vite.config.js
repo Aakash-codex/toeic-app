69f423c4fd9dbcb58b2bb4b0c1ad5b1307b800f0
Ghorasaini Aakash <ghorasainiaakash25@gmail.com>
2026-08-02 10:25:24 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Set base to your GitHub repo name for GitHub Pages, e.g. "/ascent-toeic/"
-// Keep "/" if deploying to Vercel/Netlify or a custom domain.
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-});
+  base: '/toeic-app/', // <--- Add your repo name here
+})
